6aa82815e0955d1c55f8845c49a89e6bf37cad0e
akalesh tanwer <akalesh.aurasoft@gmail.com>
2021-12-08 17:30:50 +0530

    created Slot Matchine Game

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import SlotM from "./Components/SlotM";
+function App() {
+    return (
+        <>
+            <h1>🎰 Welcome to <span style={{ color: "red" }}> Slot Machine Game </span> 🎰 </h1>
+            <div className="container">
+                <div className="slotContainer">
+                    <SlotM x="😘" y="😘" z="😘" />
+                    <SlotM x="😜" y="😘" z="😘" />
+                    <SlotM x="😘" y="😘" z="👀✔" />
+                </div>
+            </div>
+
+        </>
+    )
+};
+
+
+export default App;
---
+++ src/Components/DisMatched.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DisMatched = () =>{
+    return ( 
+        <>
+            <h2>This is not matched</h2>
+        </>
+    )
+}
+
+export default DisMatched;
