1abd5edd0ac8107ab092ca6f98ec65b582d9b674
akalesh tanwer <akalesh.aurasoft@gmail.com>
2021-12-16 18:00:12 +0530

    created a simple form

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import React, { useState } from "react";
 
 const App = () => {
-    let [text, setText] = useState();
-    let onChange = (e) => {
-        text = e.target.value;
+    let [firstName, setFirstName] = useState();
+    let [secondName, setSecondName] = useState();
+    let changeFirstName = (e) => {
+        firstName = e.target.value;
     }
-    let onClick = () => {
-        setText(text);
-        document.getElementById('greeting').value = '';
+    let changeSecondName = (e)=>{
+        secondName = e.target.value;
+
+    }
+    let onClick = (e) => {
+        e.preventDefault();
+        setFirstName(firstName);
+        setSecondName(secondName);
     }
 
     return (
         <>
-            <h2>Hello {text}</h2>
-            <div id="form">
-                <input id="greeting" type='text' onChange={onChange}></input>
-                <button onClick={onClick} >Click Me</button>
-            </div>
+            <h2>Hello {firstName} {secondName}</h2>
+            <form id="form" onSubmit={onClick}>
+                <input className="greeting" type='text' onChange={changeFirstName} placeholder="Enter Your First Name"></input>
+                <input className="greeting" type='text' placeholder="Enter Your Second Name" onChange={changeSecondName}></input>
+                <button  >Click Me</button>
+            </form>
         </>
     );
 };
